8fdb3602657d5b54cba9081514f9d6e1c28aaa3e
Hanno Böck <990588+hannob@users.noreply.github.com>
2026-03-04 11:47:55 +0100

    Avoid crashing on window resize (Apache SIGWINCH bug)

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+# SPDX-License-Identifier: 0BSD
 
 mysqld --user=root &
 
 echo "\$cfg['blowfish_secret'] ='$(head -c 20 /dev/urandom | base64)';" >>/var/www/html/config.inc.php
-apachectl -D FOREGROUND
+
+apachectl start
+
+sleep inf
